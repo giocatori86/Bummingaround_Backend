7e72a94e2035d1742130d9247446d79686920a2c
Matteo De Carlo <matteo.dek@gmail.com>
2015-03-19 17:00:27 +0100

    added Venue model

--- find_places/models.py
+++ find_places/models.py
@@ -7,3 +7,21 @@ class Point:
     def __init__(self, lat, lon):
         self.lat = lat
         self.lon = lon
+
+
+class Venue:
+    def __init__(self, _id, lat, long, name, address):
+        self.id = _id
+        self.lat = lat
+        self.long = long
+        self.name = name
+        self.address = address
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'lat': self.lat,
+            'long': self.long,
+            'name': self.name,
+            'address': self.address
+        }
