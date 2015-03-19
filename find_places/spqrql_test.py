827fcee276e2af52eb447560b4394305d154f8da
Matteo De Carlo <matteo.dek@gmail.com>
2015-03-19 19:42:06 +0100

    test to add new triples in our dataset

--- find_places/spqrql_test.py
+++ find_places/spqrql_test.py
@@ -1,4 +1,5 @@
-from find_places.graphdb import GraphDB, GRAPHDB_LINKEDGEODATA_URL
+from rdflib import RDF, Literal
+from find_places.graphdb import GraphDB, GRAPHDB_LINKEDGEODATA_URL, define_namespaces, NAMESPACES
 
 __author__ = 'Sander'
 
@@ -17,7 +18,20 @@ def make_request():
 query_result = make_request()
 
 g = query_result.get_graph()
-g.serialize("output.rdf", format="turtle")
+
+define_namespaces(g)
+
+lgdo = NAMESPACES['lgdo']
+
+for venue in g.subjects(RDF.type, lgdo.Amenity):
+    street = g.value(venue, lgdo['addr%3Astreet'])
+    house_number = g.value(venue, lgdo['addr%3Ahousenumber'])
+    if (street is not None) and (house_number is not None):
+        address = "{} {}".format(street, house_number)
+        g.add((venue, NAMESPACES['iwa'].address, Literal(address)))
+
+ttl = g.serialize("output.rdf", format="turtle")
+ttl = g.serialize(format="turtle")
 
 db = GraphDB()
-db.add_turtle(query_result.get_string())
+db.add_turtle(ttl)
