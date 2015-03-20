52bf443611d14d576aa3da81ee6434e7c21ef8f3
Matteo De Carlo <matteo.dek@gmail.com>
2015-03-20 12:19:30 +0100

    overcomplicated query

--- find_places/spqrql.py
+++ find_places/spqrql.py
@@ -21,14 +21,19 @@ PREFIX lgdo: <http://linkedgeodata.org/ontology/>
 PREFIX lgd-meta: <http://linkedgeodata.org/meta/>
 PREFIX iwa: <https://www.example.com/iwa/>
 
-SELECT ?id ?label ?lat ?lon
+SELECT DISTINCT ?id (SAMPLE(?_label) AS ?label) (SAMPLE(?_lat) as ?lat) (SAMPLE(?_lon) as ?lon)
 WHERE {{
  ?id a lgd-meta:Node ;
-    rdfs:label ?label ;
-    iwa:lat ?lat;
-    iwa:lng ?lon.
-  FILTER( ({2}-xsd:float(?lat))*({2}-xsd:float(?lat)) + ({3}-xsd:float(?lon))*({3}-xsd:float(?lon))*({0}-({1}*xsd:float(?lat))) < {4} ) .
+    rdfs:label ?_label ;
+    iwa:lat ?_lat;
+    iwa:lng ?_lon.
+  FILTER( ({2}-xsd:float(?_lat))*({2}-xsd:float(?_lat)) + ({3}-xsd:float(?_lon))*({3}-xsd:float(?_lon))*({0}-({1}*xsd:float(?_lat))) < {4} ) .
+  FILTER not exists {{
+    ?id (owl:sameAs|^owl:sameAs)* ?id_
+    filter( str(?id_) < str(?id) )
+  }}.
 }}
+GROUP BY ?id
 """
 
 GEODATA_BASE_QUERY_DESCRIBE = """
